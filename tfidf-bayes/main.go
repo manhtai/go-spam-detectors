e4ea850faced56e4206d77411a1de839bf1ef185
manhtai <vo@manhtai.com>
2021-09-07 11:05:42 +0700

    Format output

--- tfidf-bayes/main.go
+++ tfidf-bayes/main.go
@@ -7,36 +7,25 @@ import (
 
 func main() {
 	typ := "lemm_stop"
-	examples, err := ingest(typ)
+	total, err := ingest(typ)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	fmt.Printf("Examples loaded: %d\n", len(examples))
-	shuffle(examples)
-	cvStart := len(examples) - len(examples)/3
-	cv := examples[cvStart:]
-	examples = examples[:cvStart]
+	fmt.Printf("Total loaded: %d\n", len(total))
+	shuffle(total)
+	cvStart := len(total) - len(total)/3
+	test := total[cvStart:]
+	train := total[:cvStart]
 
 	c := New()
-	c.Train(examples)
+	c.Train(train)
+	fmt.Printf("Samples trained: %d\n", len(train))
 
-	var corrects, totals float64
-	for _, ex := range examples {
-		// fmt.Printf("%v", c.Score(ham.Document))
-		class := c.Predict(ex.Document)
-		if class == ex.Class {
-			corrects++
-		}
-		totals++
-	}
-	fmt.Printf("Dataset: %q. Corrects: %v, Totals: %v. Accuracy %v\n", typ, corrects, totals, corrects/totals)
-
-	fmt.Println("Start Cross Validation (this classifier)")
-	corrects, totals = 0, 0
+	corrects, totals := 0, 0
 	hams, spams := 0.0, 0.0
 	var unseen, totalWords int
-	for _, ex := range cv {
+	for _, ex := range test {
 		totalWords += len(ex.Document)
 		unseen += c.unseens(ex.Document)
 		class := c.Predict(ex.Document)
@@ -52,7 +41,8 @@ func main() {
 		totals++
 	}
 
-	fmt.Printf("Dataset: %q. Corrects: %v, Totals: %v. Accuracy %v\n", typ, corrects, totals, corrects/totals)
+	fmt.Printf("Samples tested: %d\n", len(test))
+	fmt.Printf("Dataset: %q. Corrects: %v, Totals: %v. Accuracy: %v\n", typ, corrects, totals, float32(corrects)/float32(totals))
 	fmt.Printf("Hams: %v, Spams: %v. Ratio to beat: %v\n", hams, spams, hams/(hams+spams))
 	fmt.Printf("Previously unseen %d. Total Words %d\n", unseen, totalWords)
 }
