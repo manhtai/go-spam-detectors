2554313a4c4ddddc6e5e1e48d2e942c2bad8ea1b
manhtai <vo@manhtai.com>
2021-09-08 21:38:53 +0700

    Add bayes3

---
+++ bayes3/classifier.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"github.com/navossoc/bayesian"
+	"go-spam-detector/common"
+)
+
+type bayesClassifier struct {
+	classifier *bayesian.Classifier
+}
+
+const (
+	Spam bayesian.Class = "Spam"
+	Ham  bayesian.Class = "Ham"
+)
+
+func NewBayes3Classifier() *bayesClassifier {
+	classifier := bayesian.NewClassifierTfIdf(Spam, Ham)
+	return &bayesClassifier{
+		classifier: classifier,
+	}
+}
+
+func fromClass(s common.Class) bayesian.Class {
+	if s == common.Spam {
+		return Spam
+	}
+	return Ham
+}
+
+func (b *bayesClassifier) Train(samples []common.Sample) {
+	for _, sample := range samples {
+		b.classifier.Learn(sample.Content, fromClass(sample.Class))
+	}
+	b.classifier.ConvertTermsFreqToTfIdf()
+}
+
+func (b *bayesClassifier) Predict(s []string) common.Class {
+	_, likely, _ := b.classifier.LogScores(s)
+	return []common.Class{common.Spam, common.Ham}[likely]
+}
--- go.mod
+++ go.mod
@@ -8,6 +8,7 @@ require (
 	github.com/chewxy/lingo v0.0.0-20200918122423-491e816b48d4
 	github.com/chewxy/math32 v1.0.7-0.20210223031236-a3549c8cb6a9 // indirect
 	github.com/go-nlp/tfidf v1.1.0
+	github.com/navossoc/bayesian v0.0.0-20171203014413-18fc5ea11e24
 	golang.org/x/exp v0.0.0-20210903233438-a2d0902c3ac7 // indirect
 	golang.org/x/text v0.3.7 // indirect
 	gonum.org/v1/gonum v0.9.3 // indirect
